7f40d1dc67fd90ce2657fdfd66d6028b635e3f53
Jose Cabrera Flores <cabrera.student@gmail.com>
2020-12-01 14:13:56 -0800

    Started work on script to automatically set up a day's empty files

---
+++ set_up_day.py
@@ -0,0 +1,12 @@
+import pathlib
+
+def create_folder():
+	pass
+
+def create_files():
+	pass
+	
+if __name__ == "__main__":
+	current_work_dir = pathlib.Path().absolute()
+	create_folder()
+	create_files()
