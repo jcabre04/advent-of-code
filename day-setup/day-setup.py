dbaa4388bf84841d6ee31178e79d77efad90feab
jcabre04 <cabrera.student@gmail.com>
2022-09-02 20:57:22 -0700

    Create cli tool to setup a given year's day's default files

---
+++ day-setup/day-setup.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+import argparse
+
+from templates import RUN_DEFAULT, INPUT_DEFAULT, TEST_DEFAULT
+
+
+def validate_date(year: str, day: str) -> None:
+    "Ensures year and day fit between expected ranges"
+    if not 2015 <= int(year) <= 2021:
+        raise Exception(
+            "Year is out of range. \
+            Valid range (inclusuive): 2015-2021"
+        )
+    if not 1 <= int(day) <= 25:
+        raise Exception(
+            "Day is out of range. \
+            Valid range (inclusive): 1-25"
+        )
+
+
+def create_file(p_obj: Path, content: str) -> None:
+    "Create a file with its contents if it doesn't exist"
+    if not p_obj.is_file():
+        p_obj.write_text(content)
+
+
+def create(year: str, day: str) -> None:
+    "Create the appropriate directories and files for given day"
+    src_dir = "advent_of_code"
+    test_dir = "tests"
+    year = f"year_{year}"
+    day = f"day_{day}"
+
+    year_dir = Path(f"{src_dir}/{year}")
+    year_test = Path(f"{test_dir}/{year}")
+    day_dir = year_dir / day
+
+    year_dir.mkdir(exist_ok=True)
+    year_test.mkdir(exist_ok=True)
+    day_dir.mkdir(exist_ok=True)
+
+    create_file(day_dir / "run.py", RUN_DEFAULT)
+    create_file(day_dir / "input.txt", INPUT_DEFAULT)
+    create_file(year_test / f"{day}_test.py", TEST_DEFAULT.format(year, day))
+
+
+if __name__ == "__main__":
+    """Cli interface for direct execution"""
+    parser = argparse.ArgumentParser(
+        description="""Setup given day with default run and test files""",
+    )
+
+    parser.add_argument(
+        "-y",
+        "--year",
+        required=True,
+        help="Year to setup",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--day",
+        required=True,
+        help="Day to setup",
+    )
+
+    args = parser.parse_args()
+    validate_date(args.year, args.day)
+    create(args.year, args.day)
---
+++ day-setup/templates.py
@@ -0,0 +1,35 @@
+RUN_DEFAULT = """def part1(input: list[str]) -> int:
+    "Part 1"
+    return 0
+
+
+def part2(input: list[str]) -> int:
+    "Part 2"
+    return 0
+
+
+if __name__ == "__main__":
+    input_file = "advent_of_code/year_2015/day_1/input.txt"
+    with open(input_file, "r") as file:
+        input = [line.strip() for line in file.readlines()]
+
+    print(f"Part 1: {part1(input)}")
+    print(f"Part 2: {part2(input)}")
+"""
+
+INPUT_DEFAULT = """"""
+
+TEST_DEFAULT = """from advent_of_code.{}.{} import run
+
+
+class TestPart1:
+    def test_part1_example(self) -> None:
+        # assert run.part1("(())") == 0
+        pass
+
+
+class TestPart2:
+    def test_part2_example(self) -> None:
+        # assert run.part2(")") == 1
+        pass
+"""
