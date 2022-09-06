666e5a61c592892c9d4e76a1a937d3f3ca318bdc
jcabre04 <cabrera.student@gmail.com>
2022-09-05 19:30:48 -0700

    Complete 2015-03

---
+++ advent_of_code/year_2015/day_3/run.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+
+def move(loc: list[int], dir: str) -> None:
+    "Move the given location 1 unit to the given direction"
+    match dir:
+        case "^":
+            loc[1] += 1
+        case "v":
+            loc[1] -= 1
+        case "<":
+            loc[0] -= 1
+        case ">":
+            loc[0] += 1
+
+
+def part1(input: list[str]) -> int:
+    "Count how many houses received at least one present w/ 1 deliverer"
+
+    visited = defaultdict(lambda: 0)
+    loc = [0, 0]
+
+    visited[(loc[0], loc[1])] += 1
+    for direction in input[0]:
+        move(loc, direction)
+        visited[(loc[0], loc[1])] += 1
+
+    return len(visited)
+
+
+def part2(input: list[str]) -> int:
+    "Count how many houses received at least one present w/ 2 deliverers"
+
+    visited = defaultdict(lambda: 0)
+    santa_loc = [0, 0]
+    robot_loc = [0, 0]
+
+    visited[santa_loc[0], santa_loc[1]] += 1
+    for turn, direction in enumerate(input[0]):
+        if turn % 2:
+            move(robot_loc, direction)
+            visited[robot_loc[0], robot_loc[1]] += 1
+        else:
+            move(santa_loc, direction)
+            visited[santa_loc[0], santa_loc[1]] += 1
+
+    return len(visited)
+
+
+if __name__ == "__main__":
+    input_file = "advent_of_code/year_2015/day_3/input.txt"
+    with open(input_file, "r") as file:
+        input = [line.strip() for line in file.readlines()]
+
+    print(f"Part 1: {part1(input)}")
+    print(f"Part 2: {part2(input)}")
---
+++ tests/year_2015/day_3_test.py
@@ -0,0 +1,15 @@
+from advent_of_code.year_2015.day_3 import run
+
+
+class TestPart1:
+    def test_part1_example(self) -> None:
+        assert run.part1([">"]) == 2
+        assert run.part1(["^>v<"]) == 4
+        assert run.part1(["^v^v^v^v^v"]) == 2
+
+
+class TestPart2:
+    def test_part2_example(self) -> None:
+        assert run.part2(["^v"]) == 3
+        assert run.part2(["^>v<"]) == 3
+        assert run.part2(["^v^v^v^v^v"]) == 11
